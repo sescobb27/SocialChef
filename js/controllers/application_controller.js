c20034d516da587f78be65bd6859c603c537ea4f
jmejiaz <jmejiaz@eafit.edu.co>
2014-03-16 13:06:33 -0500

    users and category search

--- js/controllers/application_controller.js
+++ js/controllers/application_controller.js
@@ -2,11 +2,34 @@ SocialChef.ApplicationController = Ember.ObjectController.extend({
     actions: {
         search: function (search_query) {
             var product = products.findBy('title', search_query.query);
-            if (product === null) {
+            if (product == null) {
                  product = products.findBy('category', search_query.query);
-             }
-             alert(product.title + ": " + product.price);
-             return product;
+                if (product == null) {
+                    product = products.findBy('cheff', search_query.query);
+
+                    if (product == null) {
+                        alert("nothing found");
+                    }
+                    else{
+                        alert(product.title + ": " + product.price);
+                        return product;
+                    }
+                }
+                else{
+                    alert(product.title + ": " + product.price);
+                    return product;
+                }
+
+            }
+
+                else{
+                    alert(product.title + ": " + product.price);
+                    return product;
+                }
+
+
+
+
         }
     }
 });
