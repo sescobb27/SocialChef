a2a327d5a46703c6def08ea2ba4829fd42d77bcd
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-04-17 01:11:19 -0500

    A user can Create, Delete, See and Update his own products

--- js/router.js
+++ js/router.js
@@ -3,8 +3,15 @@ SocialChef.Router.map(function() {
     this.route('login');
     this.route('register');
     this.resource( 'index', { path: '/' } );
-    this.resource( 'users', { path: '/chefs'} );
-    this.resource( 'user', { path: ':user_name'} );
+    this.resource( 'chefs', { path: '/chefs'} );
+    this.resource( 'users', function() {
+        this.resource( 'user', { path: '/account/:username' } );
+        this.resource( 'user_products', function() {
+            this.route( 'add' );
+            this.route( 'edit' );
+            this.route( 'delete' );
+        } );
+    });
     this.resource( 'products', function () {
         this.resource( 'product', { path: ':product_name' } );
     });
@@ -16,18 +23,34 @@ SocialChef.Router.map(function() {
 SocialChef.UsersRoute = Ember.Route.extend({
     model: function (params) {
         console.log(params);
-  }
+    }
 });
+
 SocialChef.UserRoute = Ember.Route.extend({
+    // activate: function() {},
+    // deactivate: function() {},
+    // setupController: function(controller, model) {},
+    // renderTemplate: function() {},
+    // beforeModel: function() {},
+    // afterModel: function() {},
+    setupController: function(controller, model) {
+        // Ember.$.getJSON("http://localhost:8080/", {search_value: model.query})
+        //     .then(function(products) {
+        //             controller.set('model', products);
+        //     });
+    }
+});
+
+SocialChef.ChefsRoute = Ember.Route.extend({
     model: function (params) {
         console.log(params);
-  }
+    }
 });
 
 SocialChef.IndexRoute = Ember.Route.extend({
     model: function(params) {
         console.log(params);
-  }
+    }
 });
 
 SocialChef.SearchRoute = Ember.Route.extend({
@@ -80,4 +103,60 @@ SocialChef.ProductRoute = Ember.Route.extend({
         console.log("On Product Route: " + params);
         // return this.store.find('product', params.id);
     }
+});
+
+SocialChef.UserProductsAddRoute = Em.Route.extend({
+  // activate: function() {},
+  // deactivate: function() {},
+  // setupController: function(controller, model) {},
+  // renderTemplate: function() {},
+  // beforeModel: function() {},
+  // afterModel: function() {},
+
+  model: function() {
+      return ;
+  }
+});
+
+SocialChef.UserProductsEditRoute = Em.Route.extend({
+  // activate: function() {},
+  // deactivate: function() {},
+  // setupController: function(controller, model) {},
+  // renderTemplate: function() {},
+  // beforeModel: function() {},
+  // afterModel: function() {},
+
+  model: function() {
+      return ;
+  }
+});
+
+SocialChef.UserProductsDeleteRoute = Em.Route.extend({
+  // activate: function() {},
+  // deactivate: function() {},
+  // setupController: function(controller, model) {},
+  // renderTemplate: function() {},
+  // beforeModel: function() {},
+  // afterModel: function() {},
+
+  model: function() {
+      return ;
+  }
+});
+
+SocialChef.UserProductsIndexRoute = Em.Route.extend({
+  // activate: function() {},
+  // deactivate: function() {},
+  setupController: function(controller, model) {
+      Ember.$.getJSON("http://localhost:8080/chefs/listproducts").then(function(products) {
+              controller.set('model', products);
+      });
+  },
+  // renderTemplate: function() {},
+  // beforeModel: function() {},
+  // afterModel: function() {},
+
+  model: function() {
+      return ;
+  }
 });
