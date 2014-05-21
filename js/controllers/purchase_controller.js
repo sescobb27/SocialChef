1448534caf82e1f0f9c0fe550e571d35640f5d04
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2014-05-21 12:41:43 -0500

    Applying front end template to the views, register bug fixed and purchase flow

--- js/controllers/purchase_controller.js
+++ js/controllers/purchase_controller.js
@@ -1,34 +1,50 @@
 SocialChef.PurchaseController = Ember.ObjectController.extend({
-  needs: ['product'],
   product: null,
   isProcessing: false,
+  address: '',
 
   actions: {
       purchase: function() {
           var self = this;
+          this.set('isProcessing', true);
           var promise = Ember.$.ajax({
               type: 'POST',
               url: "/purchase",
               contentType: "application/json",
               dataType: "json",
-              data: JSON.stringify(self.get('product'))
+              data: JSON.stringify(self.serialize())
           });
-          promise.success(function(){
+          promise.success(function(response){
               Ember.run(function(){
-                  self.success();
+                  self.success(response);
               });
           });
-          promise.fail(function(){
+          promise.fail(function(response){
               Ember.run(function(){
-                  self.set('isProcessing', false);
+                  self.failure(response);
               });
            });
       }
   },
-  success: function() {
-
+  success: function(response) {
+      alert(response.msg);
+      this.setProperties({
+          product: null,
+          isProcessing: false,
+          address: ''});
+      this.transitionToRoute('index');
   },
   failure: function(response) {
       this.set('isProcessing', false);
+      alert(response.errors);
+  },
+  serialize: function() {
+      var product = this.get('product');
+      return {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          entrega: this.get('address')
+      };
   }
 });
